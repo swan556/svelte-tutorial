0238f96b5e3ad1ffdb502cb350e5a560ddea44af
Swan <swanand556@gmail.com>
2025-12-06 22:59:48 +0530

    basic stuff

---
+++ src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import Clock from './clock.svelte';
+	let showclock = $state(true);
+</script>
+
+{#if showclock}
+	<Clock />
+{/if}
+
+<button
+	onclick={() => {
+		showclock = !showclock;
+	}}
+>
+	show/hide clock
+</button>
---
+++ src/routes/clock.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { onMount, onDestroy } from 'svelte';
+	let intervalId: ReturnType<typeof setInterval>;
+	let time = $state(0);
+	const updateTime = () => {
+		time++;
+	};
+	onMount(() => {
+		intervalId = setInterval(() => {
+			updateTime();
+		}, 1000);
+		console.log(`mounted at time = ${$state.snapshot(time)}`);
+	});
+
+	onDestroy(() => {
+		clearInterval(intervalId);
+		console.log(`unmounted at time = ${$state.snapshot(time)}`);
+	});
+</script>
+
+<h1>time: {time} seconds</h1>
