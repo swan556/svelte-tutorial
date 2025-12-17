8d68eeb00eb7ca8fbfab1bfbed24a559892dd851
Swan <swanand556@gmail.com>
2025-12-17 19:30:43 +0530

    did stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,20 @@
 <script lang="ts">
-	import Clock from './clock.svelte';
-	let showclock = $state(true);
 </script>
 
-{#if showclock}
-	<Clock />
-{/if}
+<button class="object" onkeypress={(key) => console.log('pressed', key)}> hello </button>
 
-<button
-	onclick={() => {
-		showclock = !showclock;
-	}}
->
-	show/hide clock
-</button>
+<style>
+	:global(body) {
+		height: 100dvh;
+		width: 100dvw;
+		background-color: rgb(58, 58, 58);
+		display: flex;
+		justify-content: center;
+		align-items: center;
+	}
+	.object {
+		width: 200px;
+		height: 100px;
+		background-color: grey;
+	}
+</style>
